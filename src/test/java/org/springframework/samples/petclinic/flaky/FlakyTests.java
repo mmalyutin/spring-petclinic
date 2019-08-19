3d3e2336457ebcd2b1dd01951b0cb93fe6bdd241
Anton Arhipov <ant.arhipov@gmail.com>
2019-08-19 15:05:18 +0300

    adding some metadata to display

--- src/test/java/org/springframework/samples/petclinic/flaky/FlakyTests.java
+++ src/test/java/org/springframework/samples/petclinic/flaky/FlakyTests.java
@@ -1,9 +1,12 @@
 package org.springframework.samples.petclinic.flaky;
 
+import org.aspectj.apache.bcel.classfile.Code;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Random;
+
+import static java.lang.String.format;
 
 public class FlakyTests {
 
@@ -11,6 +14,7 @@ public class FlakyTests {
   public void randomTest1() {
     Random r = new Random();
     int i = r.nextInt(10);
+    System.out.println(format("##teamcity[testMetadata key='randomTest1 value' type='number' value='%d']", i));
     Assert.assertTrue(i < 4);
   }
 
@@ -18,6 +22,7 @@ public class FlakyTests {
   public void randomTest2() {
     Random r = new Random();
     int i = r.nextInt(10);
+    System.out.println(format("##teamcity[testMetadata key='randomTest2 value' type='number' value='%d']", i));
     Assert.assertTrue(i < 5);
   }
 
@@ -25,6 +30,7 @@ public class FlakyTests {
   public void randomTest3() {
     Random r = new Random();
     int i = r.nextInt(10);
+    System.out.println(format("##teamcity[testMetadata key='randomTest3 value' type='number' value='%d']", i));
     Assert.assertTrue(i < 6);
   }
 }
